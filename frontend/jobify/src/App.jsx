3fd6ca0da91b119744d5642db211c491219f4db3
Hakan Bilgic <hbilgic1992@gmail.com>
2022-06-25 13:23:27 +0200

    landing page

--- frontend/jobify/src/App.jsx
+++ frontend/jobify/src/App.jsx
@@ -1,7 +1,9 @@
+import Landing from "./pages/Landing";
+
 const App = () => {
   return (
     <div>
-      <h1>Jobs</h1>
+      <Landing />
     </div>
   );
 };
---
+++ frontend/jobify/src/pages/Landing.jsx
@@ -0,0 +1,71 @@
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+import styled from "styled-components";
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  h1 {
+    font-weight: 700;
+    text-transform: capitalize;
+    span {
+      color: var(--primary-500);
+    }
+  }
+  p {
+    color: var(--grey-600);
+  }
+  .main-img {
+    display: none;
+  }
+
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`;
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* Info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app{" "}
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            libero, ut placeat neque cumque nesciunt voluptatibus officiis
+            minus, in iste totam, ullam ipsa temporibus quod necessitatibus
+            dolorem qui odio natus?
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        {/* Image */}
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+export default Landing;

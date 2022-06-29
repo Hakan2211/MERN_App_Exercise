29ab6d7e4d0d15210e500298903a681729aa6248
Hakan Bilgic <hbilgic1992@gmail.com>
2022-06-29 09:43:25 +0200

    nested pages and protected route

---
+++ frontend/jobify/src/pages/dashboard/SharedLayout.jsx
@@ -0,0 +1,35 @@
+import { Outlet, Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  .dashboard {
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+  .dashboard-page {
+    width: 90vw;
+    margin: 0 auto;
+    padding: 2rem 0;
+  }
+  @media (min-width: 992px) {
+    .dashboard {
+      grid-template-columns: auto 1fr;
+    }
+    .dashboard-page {
+      width: 90%;
+    }
+  }
+`;
+
+const SharedLayout = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Link to="add-job">Add Job</Link>
+        <Link to="all-jobs">All Jobs</Link>
+      </nav>
+      <Outlet />
+    </Wrapper>
+  );
+};
+export default SharedLayout;
